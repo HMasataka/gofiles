2bb94d6d5e031ddaa802b1ee1a66db87d4b485f6
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-02-02 20:58:44 +0900

    Feat use dir perm

--- create.go
+++ create.go
@@ -6,9 +6,9 @@ func CreateWriteFile(path string) (*os.File, error) {
 	return os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
 }
 
-func CreateDirectoryIfNotExist(path string, perm os.FileMode) error {
+func CreateDirectoryIfNotExist(path string) error {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		if err := os.Mkdir(path, perm); err != nil {
+		if err := os.Mkdir(path, os.ModeDir); err != nil {
 			return err
 		}
 	}
