2359694626eb9124089473c74343875f8b9c08d7
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-02-02 20:53:19 +0900

    Feat create directory

--- create.go
+++ create.go
@@ -5,3 +5,13 @@ import "os"
 func CreateWriteFile(path string) (*os.File, error) {
 	return os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
 }
+
+func CreateDirectoryIfNotExist(path string, perm os.FileMode) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		if err := os.Mkdir(path, perm); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
