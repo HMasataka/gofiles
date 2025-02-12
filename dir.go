62cde0d3ed0ec5da635386d432c94167d913c4eb
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-02-12 14:59:00 +0900

    Feat list files

--- dir.go
+++ dir.go
@@ -8,3 +8,38 @@ import (
 func Pwd() (string, error) {
 	return filepath.Abs(filepath.Dir(os.Args[0]))
 }
+
+func ListFiles(path string) ([]string, error) {
+	var filePaths []string
+
+	if err := filepath.Walk(path, func(path string, info os.FileInfo, e error) error {
+		if e != nil {
+			return e
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		filePaths = append(filePaths, path)
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return filePaths, nil
+}
+
+func ListFilesYield(path string) (func(func(string) bool), error) {
+	filePaths, err := ListFiles(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return func(yield func(string) bool) {
+		for _, filePath := range filePaths {
+			yield(filePath)
+		}
+	}, nil
+}
