a6547237753724b09874b2df4dc40cbf3b5f6afd
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-02-12 15:03:01 +0900

    Refactor split file

--- dir.go
+++ dir.go
@@ -9,6 +9,16 @@ func Pwd() (string, error) {
 	return filepath.Abs(filepath.Dir(os.Args[0]))
 }
 
+func CreateDirectoryIfNotExist(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		if err := os.Mkdir(path, 0755); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func ListFiles(path string) ([]string, error) {
 	var filePaths []string
 
--- file.go
+++ file.go
@@ -7,6 +7,10 @@ import (
 	"path/filepath"
 )
 
+func CreateWriteFile(path string) (*os.File, error) {
+	return os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
+}
+
 func CopyFile(src string, dst string) error {
 	srcInfo, err := os.Stat(src)
 	if err != nil {
